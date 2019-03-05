270dcc9e1feab60e9b7e0ddf411649c657173ced
skyrain1995 <2503123003@qq.com>
2019-03-06 00:22:33 +0800

    树的基本定义

--- src/tree/TreeNode.java
+++ src/tree/TreeNode.java
@@ -7,7 +7,6 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
-    public TreeNode parent;
 
     public TreeNode(int val){
         this.val = val;
