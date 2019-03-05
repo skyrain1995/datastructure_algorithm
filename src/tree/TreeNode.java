bf29daa31d1509d0f0b217e752bb40088f5533d9
skyrain1995 <2503123003@qq.com>
2019-03-05 21:55:01 +0800

    树的基本定义

---
+++ src/tree/TreeNode.java
@@ -0,0 +1,14 @@
+package tree;
+
+/**
+ * 树的基本定义
+ */
+public class TreeNode {
+    private int val;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(int val){
+        this.val = val;
+    }
+}
